5a965c67f4333c22bb1836ae71adc7c2736a5f36
Nouran-Ebrahim <nouran.ssp@gmail.com>
2024-11-17 14:41:18 +0200

    them

---
+++ resources/views/layouts/dashboard/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}" data-textdirection="{{ LaravelLocalization::getCurrentLocaleDirection() }}"
+    class="loading">
+
+<head>
+    @include('layouts.dashboard._head')
+</head>
+
+<body class="vertical-layout vertical-menu-modern 2-columns   menu-expanded fixed-navbar" data-open="click"
+    data-menu="vertical-menu-modern" data-col="2-columns">
+    <!-- fixed-top-->
+    @include('layouts.dashboard._header')
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+    @include('layouts.dashboard._sidebar')
+    @yield('content')
+
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+    @include('layouts.dashboard._footer')
+    @include('layouts.dashboard._scripts')
+</body>
+
+</html>
